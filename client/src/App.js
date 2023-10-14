0885f9b4bbb983088cc616e765737883853c5705
Rabbani2019331039 <fathoor39@student.sust.edu>
2023-10-15 04:56:52 +0600

    Add Sign Up Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Pages/Homepage/Homepage";
+import Signup from "./components/Pages/SignUp/SIgnup";
 import AdminDashboard from "./pages/AdminDashboard";
 import Blog from "./pages/Blog";
 
@@ -11,6 +12,7 @@ function App() {
 				<Route path="/" element={<Homepage />} />
 				<Route path="admin-dashboard" element={<AdminDashboard />} />
 				<Route path="blog" element={<Blog />} />
+				<Route path="signup" element={<Signup />} />
 			</Routes>
 		</Router>
 	);
