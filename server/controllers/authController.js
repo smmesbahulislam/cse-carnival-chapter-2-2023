d3ce1349ecfbf81b2b3a459d2488d495d5eb4e6e
SM Mesbahul Islam <mesbahulislam206@gmail.com>
2023-10-14 11:54:10 +0600

    Add Authentication System

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,10 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelpers.js";
+import { hashPassword, comparePassword } from "../helpers/authHelpers.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async( req, res) => {
     try {
-        const {name,email,password,phone,secretKey} = req.body;
+        const {name,email,password,phone,secretKey, role} = req.body;
         //validation
         if(!name){
             return res.send({message:"Name is Required"});
@@ -14,11 +15,11 @@ export const registerController = async( req, res) => {
         if(!password){
             return res.send({message:"password is Required"});
         }
-        if(!phone){
-            return res.send({message:"phone is Required"});
-        }
         if(!secretKey){
             return res.send({message:"secretKey is Required"});
+        }
+        if(!role){
+            return res.send({message:"role is Required"});
         }
         //check user
         const existingUser = await userModel.findOne({email});
@@ -32,7 +33,7 @@ export const registerController = async( req, res) => {
         //register user
         const hashedPassword = await hashPassword(password);
         //save user
-        const user = await new userModel({name,email,phone,password:hashedPassword,secretKey}).save();
+        const user = await new userModel({name,email,phone,password:hashedPassword,secretKey,role}).save();
         res.status(201).send({
             success:true,
             message: 'User Register Successfully',
@@ -47,3 +48,97 @@ export const registerController = async( req, res) => {
         })
     }
 }
+
+export const loginController = async(req,res) =>{
+    try {
+        const {email,password} = req.body;
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:'Invalid email or password'
+            })
+        }
+        //check user
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'Email is not registered'
+            })
+        }
+        const match = await comparePassword(password,user.password);
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:'Invalid password'
+            })
+        }
+
+        //token
+        const token = await JWT.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'7d',});
+        res.status(200).send({
+            success:true,
+            message:'login successfully',
+            user:{
+                _id: user._id,
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address,
+                role: user.role,
+                secretKey: user.secretKey,
+            },
+            token,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'Error in login',
+            error
+        })
+        
+    }
+}
+
+//forgot password controller
+export const forgotPasswordController = async(req, res) => {
+    try {
+        const {email, secretKey, newPassword} = req.body
+        if(!email){
+            res.status(400).send({message: 'Email is required'})
+        }
+        if(!secretKey){
+            res.status(400).send({message: 'SecretKey is required'})
+        }
+        if(!newPassword){
+            res.status(400).send({message: 'New Password is required'});
+        }
+
+        //check 
+        const user = await userModel.findOne({email,secretKey})
+        //validation
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message: 'Wrong Email or secretKey',
+            })
+        }
+        const hashed = await hashPassword(newPassword);
+        await userModel.findByIdAndUpdate(user._id, {password: hashed});
+        res.status(200).send({
+            success: true,
+            message: "Password Reset Successfully",
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error
+        })
+        
+    }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import { registerController, loginController, forgotPasswordController } from '../controllers/authController.js';
 
 
 const router = express.Router();
@@ -7,5 +7,12 @@ const router = express.Router();
 //routing
 //Register || Method POST
 router.post('/register',registerController);
+
+//Login || POST
+router.post('/login',loginController);
+
+//Forgot password || POST
+router.post('/forgot-password',forgotPasswordController)
+
 
 export default router;
