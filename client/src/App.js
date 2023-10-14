e01ffdccbf7fbb3598a8a3400cb401e258789681
Rabbani2019331039 <fathoor39@student.sust.edu>
2023-10-14 23:52:24 +0600

    After merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,19 @@
-<<<<<<< HEAD
-import {
-	Route,
-	Routes,
-	useLocation,
-	useNavigationType,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Pages/Homepage/Homepage";
+import AdminDashboard from "./pages/AdminDashboard";
+import Blog from "./pages/Blog";
 
 function App() {
 	return (
-		<div className="App">
+		<Router>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				<Route path="admin-dashboard" element={<AdminDashboard />} />
+				<Route path="blog" element={<Blog />} />
 			</Routes>
-		</div>
+		</Router>
 	);
-=======
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminDashboard from './pages/AdminDashboard';
-import Blog from './pages/Blog';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="admin-dashboard" element={<AdminDashboard/>} />
-        <Route path="blog" element={<Blog/>} />
-      </Routes>
-    </Router>
-  );
->>>>>>> develop
 }
 
 export default App;
