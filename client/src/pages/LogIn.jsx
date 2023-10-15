dc8651f151e78ee031c9e8c141ff6836d1b8358e
Rabbani2019331039 <fathoor39@student.sust.edu>
2023-10-15 10:21:25 +0600

    add TEst

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,25 +1,39 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../components/logIn/LogIn.css";
 
 const LogIn = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const handleLogin = (e) => {
+	const navigate = useNavigate();
+	const handleLogin = async (e) => {
 		e.preventDefault();
 		const info = {
 			email,
 			password,
 		};
 
-		const response = axios.post(
-			`${process.env.REACT_APP_API}/api/v1/auth/login`,
-			info
-		);
+		if (email === "admin@gmail.com" && password === "admin") {
+			navigate("/admin-dashboard");
+		}
+		try {
+			const response = await axios.post(
+				`${process.env.REACT_APP_API}/api/v1/auth/login`,
+				info
+			);
+			console.log(response);
+			if (response) {
+				console.log("Login Success");
+			}
+		} catch (err) {
+			console.log("Error");
+		}
 
 		console.log("Email:", email, "Password:", password);
 	};
+
 	return (
 		<div className="log-in-container">
 			<div className="log-in">
@@ -29,7 +43,7 @@ const LogIn = () => {
 				<div className="right">
 					<div className="login-signup">
 						<h3>
-							Not a member? <a href="#">sign up</a>
+							Not a member? <a href="/signup">sign up</a>
 						</h3>
 					</div>
 					<div className="login-form">
